a788f8ed31b3395c8d705410645ee894423ecd25
“espelucero” <espe.lucero@gmail.com>
2017-05-05 02:41:57 -0300

    Avances

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,14 +5,44 @@ function MiembroSquad(nombreYApellido, edad, hobbies){
 
 }
 
-var Fernanda = new MiembroSquad('Fernanda Zamora', 30,
+var fernanda = new MiembroSquad('Fernanda Zamora', 30,
 	['escuchar música','Hacer fotografías','Pensar en el existencialismo']);
 
-var Valentina = new MiembroSquad('Valentina Miranda', 26,['dibujar','ver anime y series', 'cocinar']);
+var valentina = new MiembroSquad('Valentina Miranda', 26,['dibujar','ver anime y series', 'cocinar']);
 
-var Cindialy = new MiembroSquad('Cindialy Berrios', 23, ['Trekking', 'Escuchar música', 'Deporte']);
+var cindialy = new MiembroSquad('Cindialy Berrios', 23, ['Trekking', 'Escuchar música', 'Deporte']);
 
-var Diana = new MiembroSquad('Diana Villasmil', 29, ['cantar', 'ver series coreanas', 'leer']);
+var diana = new MiembroSquad('Diana Villasmil', 29, ['cantar', 'ver series coreanas', 'leer']);
 
-var Natalia = new MiembroSquad('Natalia Villalobos', 30, 'tocar batería');
+var natalia = new MiembroSquad('Natalia Villalobos', 30, 'tocar batería');
 
+var lista = [];
+
+lista.push(fernanda);
+lista.push(valentina);
+lista.push(cindialy);
+lista.push(diana);
+lista.push(natalia);
+
+lista.forEach(function(el){
+	
+})
+
+
+
+/*var lista = document.getElementsByClassName('listaIntegrantes');
+
+var miembro1 = "<b><li>Nombre y Apellido: </b>" + fernanda.nombreYApellido + "<br></li><b><li>Edad: </b>" + fernanda.edad + "<br></li><b><li>Hobbie: </b>" + fernanda.hobbies
+    + "<br><br>";
+var miembro2 = "<b><li>Nombre y Apellido: </b>" + valentina.nombreYApellido + "<br></li><b><li>Edad: </b>" + valentina.edad + "<br></li><b><li>Hobbie: </b>" + valentina.hobbies
+    + "<br><br>";
+var miembro3 = "<b><li>Nombre y Apellido: </b>" + cindialy.nombreYApellido + "<br></li><b><li>Edad: </b>" + cindialy.edad + "<br></li><b><li>Hobbie: </b>" + cindialy.hobbies
+    + "<br><br>";
+var miembro4 = "<b><li>Nombre y Apellido: </b>" + diana.nombreYApellido + "<br></li><b><li>Edad: </b>" + diana.edad + "<br></li><b><li>Hobbie: </b>" + diana.hobbies
+    + "<br><br>";
+var miembro5 = "<b><li>Nombre y Apellido: </b>" + natalia.nombreYApellido + "<br></li><b><li>Edad: </b>" + natalia.edad + "<br></li><b><li>Hobbie: </b>" + natalia.hobbies
+    + "<br><br>";
+
+var resultado = miembro1 + miembro2 + miembro3 + miembro4 + miembro5;
+
+lista.innerHTML = resultado;*/
