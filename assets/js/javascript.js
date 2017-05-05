bd0ee2f8ac3207f1d66714d353e7bff415366773
“espelucero” <espe.lucero@gmail.com>
2017-05-05 01:59:16 -0300

    Iniciando ejercicio 2: Mi squad

---
+++ assets/js/javascript.js
@@ -0,0 +1,18 @@
+function MiembroSquad(nombreYApellido, edad, hobbies){
+	this.nombreYApellido = nombreYApellido,
+	this.edad = edad,
+	this.hobbies = hobbies,
+
+}
+
+var Fernanda = new MiembroSquad('Fernanda Zamora', 30,
+	['escuchar música','Hacer fotografías','Pensar en el existencialismo']);
+
+var Valentina = new MiembroSquad('Valentina Miranda', 26,['dibujar','ver anime y series', 'cocinar']);
+
+var Cindialy = new MiembroSquad('Cindialy Berrios', 23, ['Trekking', 'Escuchar música', 'Deporte']);
+
+var Diana = new MiembroSquad('Diana Villasmil', 29, ['cantar', 'ver series coreanas', 'leer']);
+
+var Natalia = new MiembroSquad('Natalia Villalobos', 30, 'tocar batería');
+
